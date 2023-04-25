583581b39b6c7966ec4d21aae5f82197d3a4bf8a
mark <markpackham1@gmail.com>
2023-04-25 11:32:00 +0100

    clear out starter code
    https://youtu.be/PMs80Ps_OBI?t=320

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
-
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div>
+      <header>
+        <h1>Merch</h1>
       </header>
     </div>
   );
