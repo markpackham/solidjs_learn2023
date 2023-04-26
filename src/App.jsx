31fdc9a4c8d8bfc3518749780a6fd0d37bccd286
mark <markpackham1@gmail.com>
2023-04-26 16:33:13 +0100

    import site banner
    https://youtu.be/PMs80Ps_OBI?t=391

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import banner from "./assets/banner.png";
+
 function App() {
   return (
     <div>
       <header>
         <h1>Merch</h1>
       </header>
+
+      <img src={banner} alt="site banner" />
     </div>
   );
 }
