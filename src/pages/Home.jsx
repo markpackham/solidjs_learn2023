4b17027a5e153c968459d06dc817e50c8eb42983
mark <markpackham1@gmail.com>
2023-04-27 11:27:33 +0100

    use Show for products
    https://youtu.be/Ala3YtfaGBo?t=143

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { createResource } from "solid-js";
+import { Show, createResource } from "solid-js";
 import Card from "../components/Card";
 
 export default function Home() {
@@ -11,20 +11,22 @@ export default function Home() {
   const [products] = createResource(fetchProducts);
 
   return (
-    <div class="grid grid-cols-4 gap-10 my-4">
-      <Card flat={true} rounded={false}>
-        <h2>Ninja Tee, Black</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-        <button class="btn">view</button>
-      </Card>
-      <Card flat={false} rounded={true}>
-        <h2>Ninja Tee, White</h2>
-        <button class="btn">view</button>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis.</p>
-        <p>Only £10</p>
-      </Card>
-
-      <p>{console.log(products(), products.loading)}</p>
-    </div>
+    <Show where={products()} fallback={<p>Loading...</p>}>
+      <div class="grid grid-cols-4 gap-10 my-4">
+        <Card flat={true} rounded={false}>
+          <h2>Ninja Tee, Black</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+          <button class="btn">view</button>
+        </Card>
+        <Card flat={false} rounded={true}>
+          <h2>Ninja Tee, White</h2>
+          <button class="btn">view</button>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis.
+          </p>
+          <p>Only £10</p>
+        </Card>
+      </div>
+    </Show>
   );
 }
