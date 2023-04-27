9371e84207b91bc8bd9a6a181085e60a8016f30e
mark <markpackham1@gmail.com>
2023-04-27 11:32:44 +0100

    fetch the products on Home and render

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,19 +13,14 @@ export default function Home() {
   return (
     <Show where={products()} fallback={<p>Loading...</p>}>
       <div class="grid grid-cols-4 gap-10 my-4">
-        <Card flat={true} rounded={false}>
-          <h2>Ninja Tee, Black</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-          <button class="btn">view</button>
-        </Card>
-        <Card flat={false} rounded={true}>
-          <h2>Ninja Tee, White</h2>
-          <button class="btn">view</button>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis.
-          </p>
-          <p>Only £10</p>
-        </Card>
+        <For each={products()}>
+          {(product) => (
+            <Card rounded={true} flat={true}>
+              <img src={product.img} alt={product.title} />
+              <h2 class="my-3 font-bold">{product.title}</h2>
+            </Card>
+          )}
+        </For>
       </div>
     </Show>
   );
