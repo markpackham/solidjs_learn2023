1d529e4353ff3b5e34d1048f27d145f4c7900f51
mark <markpackham1@gmail.com>
2023-04-27 10:47:02 +0100

    create Home page
    https://youtu.be/7oDjB9v0zMs?t=302

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import { createSignal } from "solid-js";
 import banner from "./assets/banner.png";
 import Card from "./components/Card";
@@ -26,22 +27,9 @@ function App() {
 
       <img class="rounded-md" src={banner} alt="site banner" />
 
-      <div class="grid grid-cols-4 gap-10 my-4">
-        <Card flat={true} rounded={false}>
-          <h2>Ninja Tee, Black</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-          <button class="btn">view</button>
-        </Card>
-
-        <Card flat={false} rounded={true}>
-          <h2>Ninja Tee, White</h2>
-          <button class="btn">view</button>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis.
-          </p>
-          <p>Only £10</p>
-        </Card>
-      </div>
+      <Routes>
+        <Route path="/" component={Home} />
+      </Routes>
     </div>
   );
 }
