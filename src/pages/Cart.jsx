f5ef0cca435508db6893590703e14497a39a737f
mark <markpackham1@gmail.com>
2023-04-28 10:31:00 +0100

    show total cart price
    https://youtu.be/IiX3Y4ooGPU?t=384

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,6 +5,13 @@ import { useCartContext } from "../context/CartContext";
 export default function Cart() {
   const { items } = useCartContext();
 
+  // find how much is spent in the cart
+  const total = () => {
+    return items.reduce((accumulator, prod) => {
+      return accumulator + prod.quantity * prod.price;
+    }, 0);
+  };
+
   return (
     <div className="max-w-md my-8 mx-auto">
       <Card rounded={true}>
@@ -16,6 +23,10 @@ export default function Cart() {
             </p>
           )}
         </For>
+
+        <p className="nt-8 pt-4 border-t-2 font-bold">
+          Total cart price - £{total()}
+        </p>
       </Card>
     </div>
   );
